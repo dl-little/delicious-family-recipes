38d34830d4c13e970bdf59e3a1f70b40bbcb122b
dl-little <little.douglas22@gmail.com>
2024-04-16 15:13:44 -0400

    Create notice when settings update

--- templates/page.php
+++ templates/page.php
@@ -1,5 +1,14 @@
 <div class="wrap" id="dfr-settings-admin">
 	<h1 class="wp-heading-inline"><?php echo get_admin_page_title(); ?></h1>
+	<?php
+	if ( ! empty( $_GET ) && ! empty( $_GET[ 'settings-updated' ] ) ) {
+		?>
+		<div class="notice notice-success is-dismissible">
+			<p><strong><?php _e('Settings saved.') ?></strong></p>
+		</div>
+		<?php
+	}
+	?>
 	<form action="options.php" method="post">
 		<?php
 			settings_fields( $this->page_slug );
