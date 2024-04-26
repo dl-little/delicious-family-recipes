16c1ffb8596c5e0fa259f83590c3647812899b09
dl-little <little.douglas22@gmail.com>
2024-04-26 01:52:14 -0400

    Full-width block

---
+++ src/extensions/full-width-group/index.js
@@ -0,0 +1,35 @@
+import { registerBlockVariation } from '@wordpress/blocks';
+const MY_VARIATION_NAME = 'dfr/full-width-group';
+
+registerBlockVariation( 'core/group', {
+	name: MY_VARIATION_NAME,
+	title: 'Delicious Family Recipes Full Width Group Block',
+    description: 'Provides a background color to a group for use on the homepage.',
+    isActive: ( { namespace } ) => {
+		return (
+			namespace === MY_VARIATION_NAME
+		);
+	},
+	icon: 'align-full-width',
+    attributes: {
+		className: 'dfr-full-width-group',
+        namespace: MY_VARIATION_NAME,
+		layout: {
+			type: 'flex',
+			orientation: 'vertical',
+			justifyContent: 'center',
+		},
+		style: {
+			spacing: {
+				padding: {
+					top: '15px',
+					bottom: '15px',
+				}
+			}
+		}
+    },
+	innerBlocks: [
+		[ 'core/group' ],
+	],
+    scope: [ 'inserter' ],
+});
--- src/extensions/index.js
+++ src/extensions/index.js
@@ -2,3 +2,4 @@ import './style.scss';
 import './featured-posts';
 import './recent-posts';
 import './section-header';
+import './full-width-group';
